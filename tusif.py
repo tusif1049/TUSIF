b5df2af21e31c5cc316712e236b410d838a7cbed
tusif1049 <73287720+tusif1049@users.noreply.github.com>
2020-10-29 13:07:14 +0600

    Update tusif.py

--- tusif.py
+++ tusif.py
@@ -149,8 +149,8 @@ print  """\033[1;91m
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = TUSIF
-CorrectPassword = TUSIF
+CorrectUsername =tusif
+CorrectPassword =tusif
 
 loop = 'true'
 while (loop == 'true'):
